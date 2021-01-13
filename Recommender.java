c7135eb3346e6aefc27f16af3075f40197be720c
Rohan Goyal <48727039+RohanGoyal17@users.noreply.github.com>
2021-01-14 00:27:25 +0530

    Add files via upload

--- Recommender.java
+++ Recommender.java
@@ -42,7 +42,8 @@ class Recommender{
        			arr[i] = Integer.parseInt(str[i+5]);
        		}
        		int rating = 0;
-       		ret.put(id, new Object[]{name, date, imdb, arr, new Integer(rating)});
+       		int watches = 0;
+       		ret.put(id, new Object[]{name, date, imdb, arr, new Integer(rating), new Integer(watches)});
 		}
 		return ret;
 	}
@@ -61,12 +62,13 @@ class Recommender{
        		String gender = str[2];
        		String occ = str[3];
        		String zip = str[4];
-       		ret.put(id, new Object[]{new Integer(age), gender, occ, zip});
+       		int norat = 0;
+       		ret.put(id, new Object[]{new Integer(age), gender, occ, zip, new Integer(norat)});
 		}
 		return ret;
 	}
 
-	static void fillRatings(int[][] ratings, String fileName, Map<Integer, Object[]> movieData) throws IOException{
+	static void fillRatings(int[][] ratings, String fileName, Map<Integer, Object[]> movieData, Map<Integer, Object[]> userData, int[][] genreComp, int[][] genreData) throws IOException{
 		BufferedReader br = new BufferedReader(new FileReader(fileName));
 		// StringTokenizer st;
 		String line;
@@ -76,6 +78,14 @@ class Recommender{
 			int mId = Integer.parseInt(str[1])-1;
 			ratings[uId][mId] = Integer.parseInt(str[2]);
 			movieData.get(mId+1)[4] = new Integer((Integer)movieData.get(mId + 1)[4] + ratings[uId][mId]);
+			movieData.get(mId+1)[5] = new Integer((Integer)movieData.get(mId + 1)[5] +  1);
+			userData.get(uId+1)[4] =  new Integer((Integer)userData.get(uId+1)[4] + 1);
+			for(int k = 0; k< 19; k++){	
+				if(genreData[mId][k] == 1){
+					genreComp[k][0]++;
+					genreComp[k][1] += ratings[uId][mId];
+				}
+			}
 		}
 	}
 
@@ -88,9 +98,10 @@ class Recommender{
 		}
 		Map<Integer, Object[]> userData = getUserData("user.data");
 		int[][] ratingMatrix = new int[userData.size()][movieData.size()];
-		fillRatings(ratingMatrix, "ratings.data", movieData);
+		int[][] genreComp = new int[19][2];
+		fillRatings(ratingMatrix, "ratings.data", movieData, userData, genreComp, genreData);
 
-		System.out.println("Best movie by genre, Enter genre index (refer documents)");
+		System.out.println("1.) Best movie by genre, Enter genre index (refer documents)");
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			int comparat = Integer.parseInt(br.readLine());
 			int flag = 0;
@@ -103,11 +114,11 @@ class Recommender{
 					}
 				}
 			}
-			System.out.println(output);
+		System.out.println(output);
 			flag = 0;
 			output = "";
 
-		System.out.println("Best movie by year, Enter year");
+		System.out.println("2.) Best movie by year, Enter year");
 			String yearin = br.readLine();
 			for(int i =1; i < movieData.size()+1; i++){
 				if(((String)movieData.get(i)[1]).compareTo(yearin) == 0){
@@ -117,11 +128,11 @@ class Recommender{
 					}
 				}
 			}
-			System.out.println(output);
+		System.out.println(output);
 			flag = 0;
 			output = "";
 
-		System.out.println("Best movie by year and genre, Enter year");
+		System.out.println("3.) Best movie by year and genre, Enter year");
 			yearin = br.readLine();
 			System.out.println("Enter genre index");
 			comparat = Integer.parseInt(br.readLine());
@@ -135,10 +146,49 @@ class Recommender{
 					}
 				}
 			}
-			System.out.println(output);
+		System.out.println(output);
 			flag = 0;
 			output = "";
 
+		// most watched movie
+			for(int i =1; i < movieData.size()+1; i++){
+				if((Integer)movieData.get(i)[5]> flag){
+					flag = (Integer)movieData.get(i)[5];
+					output = (String)movieData.get(i)[0];
+				}
+			}
+
+		System.out.println("4.) Most watched movie is  " + output);
+			flag = 0;
+			output = "";
+			int outputi = 0;
+			int outputib = 0;
+
+		//most watched genre	, highest rated genre
+			float flagb = 1;
+			for(int i =0; i< 19; i++){
+				if(genreComp[i][0] > flag){
+					flag = genreComp[i][0];
+					outputi = i; 
+				}
+				if((float)genreComp[i][1]/genreComp[i][0] > flagb){
+					flagb = (float)genreComp[i][1]/genreComp[i][0];
+					outputib = i; 
+				}
+			}
+		System.out.println("5.) Most watched genre is: " + outputi);
+		System.out.println("6.) Highest rated genre is: " + outputib);
+
+		// most active user
+			flag = 0;
+			outputi = 0;
+			for(int i =1; i < userData.size()+1; i++){
+				if((Integer)userData.get(i)[4]> flag){
+					flag = (Integer)userData.get(i)[4];
+					outputi = i;
+				}
+			}
+		System.out.println("7.) Most active user is ID number: " + outputi);
 	}
 
 }
