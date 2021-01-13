e8a60fb6a328d6a9920d19b67ec8a40db0c3caf3
Rohan Goyal <48727039+RohanGoyal17@users.noreply.github.com>
2021-01-13 23:16:16 +0530

    Add files via upload

--- Recommender.java
+++ Recommender.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.util.*;
 
+
+
 class Recommender{
 
 	static String[] getGenre(String fileName) throws IOException{
@@ -20,68 +22,123 @@ class Recommender{
 
 	static Map<Integer, Object[]> getMovieData(String fileName) throws IOException{
 		BufferedReader br = new BufferedReader(new FileReader(fileName));
-		Map<String, Object[]> ret = new HashMap<>();
-		StringTokenizer st;
+		Map<Integer, Object[]> ret = new HashMap<>();
+		// StringTokenizer st;
 		String line;
 		while((line = br.readLine()) != null){
 			if(line.length() == 0)
 				continue;
-			st = new StringTokenizer(line, "|");
-			int id = Integer.parseInt(st.nextToken());
-			/*movie id | movie title | release date | video release date |
-       IMDb URL | */
-       		String name = st.nextToken();
-       		String date = st.nextToken();
-       		String imdb = st.nextToken();
+			String[] str = line.split("[|]");
+			int id = Integer.parseInt(str[0]);
+			String name = str[1];
+			String date; 
+			if(str[2].length() > 10)
+				date = str[2].split("[-]")[2];
+			else
+				date = "";
+       		String imdb = str[4];
        		int[] arr = new int[19];
-       		for(int i = 0; i < n; i++){
-       			arr[i] = Integer.parseInt(st.nextToken());
+       		for(int i = 0; i < 19; i++){
+       			arr[i] = Integer.parseInt(str[i+5]);
        		}
-       		ret.put(id, new Object[]{name, date, imdb, arr});
+       		int rating = 0;
+       		ret.put(id, new Object[]{name, date, imdb, arr, new Integer(rating)});
 		}
 		return ret;
 	}
 
-	static Map<Integer, Object[]> getMovieData(String fileName) throws IOException{
+	static Map<Integer, Object[]> getUserData(String fileName) throws IOException{
 		BufferedReader br = new BufferedReader(new FileReader(fileName));
-		Map<String, Object[]> ret = new HashMap<>();
-		StringTokenizer st;
+		Map<Integer, Object[]> ret = new HashMap<>();
+		// StringTokenizer st;
 		String line;
 		while((line = br.readLine()) != null){
 			if(line.length() == 0)
 				continue;
-			st = new StringTokenizer(line, "|");
-			int id = Integer.parseInt(st.nextToken());
-       		int age = Integer.parseInt(st.nextToken());
-       		String gender = st.nextToken();
-       		String occ = st.nextToken();
-       		String zip = st.nextToken();
+			String[] str = line.split("[|]");
+			int id = Integer.parseInt(str[0]);
+       		int age = Integer.parseInt(str[1]);
+       		String gender = str[2];
+       		String occ = str[3];
+       		String zip = str[4];
        		ret.put(id, new Object[]{new Integer(age), gender, occ, zip});
 		}
 		return ret;
 	}
 
-	static void fillRatings(int[][] ratings, String fileName) throws IOException{
+	static void fillRatings(int[][] ratings, String fileName, Map<Integer, Object[]> movieData) throws IOException{
 		BufferedReader br = new BufferedReader(new FileReader(fileName));
-		StringTokenizer st;
+		// StringTokenizer st;
 		String line;
 		while((line = br.readLine()) != null){
-			st = new StringTokenizer(line, "\t");
-			int uId = Integer.parseInt(st.nextToken())-1;
-			int mId = Integer.parseInt(st.nextToken())-1;
-			ratings[uId][mId] = Integer.parseInt(st.nextToken());
+			String[] str = line.split("[\t]");
+			int uId = Integer.parseInt(str[0])-1;
+			int mId = Integer.parseInt(str[1])-1;
+			ratings[uId][mId] = Integer.parseInt(str[2]);
+			movieData.get(mId+1)[4] = new Integer((Integer)movieData.get(mId + 1)[4] + ratings[uId][mId]);
 		}
 	}
 
 	public static void main(String[] args) throws IOException{
-		String[] genre = getGenre("");						// 	basically enum
-		Map<Integer, Object[]> movieData = getMovieData("");			// 
+		String[] genre = getGenre("genre.data");
+		Map<Integer, Object[]> movieData = getMovieData("movie.data");
 		int[][] genreData =  new int[movieData.size()][19];
 		for(int i = 0; i < genreData.length; i++){
 			genreData[i] = (int[])movieData.get(i+1)[3];
 		}
-		Map<Integer, Object[]> userData = new getUserData("");
+		Map<Integer, Object[]> userData = getUserData("user.data");
 		int[][] ratingMatrix = new int[userData.size()][movieData.size()];
-		fillRatings(ratingMatrix, "");
+		fillRatings(ratingMatrix, "ratings.data", movieData);
+
+		System.out.println("Best movie by genre, Enter genre index (refer documents)");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			int comparat = Integer.parseInt(br.readLine());
+			int flag = 0;
+			String output = "";
+			for(int i =1; i < movieData.size()+1; i++){
+				if(genreData[i-1][comparat] == 1){
+					if((Integer)movieData.get(i)[4] > flag){
+						flag = (Integer)movieData.get(i)[4];
+						output = (String)movieData.get(i)[0];
+					}
+				}
+			}
+			System.out.println(output);
+			flag = 0;
+			output = "";
+
+		System.out.println("Best movie by year, Enter year");
+			String yearin = br.readLine();
+			for(int i =1; i < movieData.size()+1; i++){
+				if(((String)movieData.get(i)[1]).compareTo(yearin) == 0){
+					if((Integer)movieData.get(i)[4] > flag){
+						flag = (Integer)movieData.get(i)[4];
+						output = (String)movieData.get(i)[0];
+					}
+				}
+			}
+			System.out.println(output);
+			flag = 0;
+			output = "";
+
+		System.out.println("Best movie by year and genre, Enter year");
+			yearin = br.readLine();
+			System.out.println("Enter genre index");
+			comparat = Integer.parseInt(br.readLine());
+			for(int i =1; i < movieData.size()+1; i++){
+				if(genreData[i-1][comparat] == 1){
+					if(((String)movieData.get(i)[1]).compareTo(yearin) == 0){
+						if((Integer)movieData.get(i)[4] > flag){
+							flag = (Integer)movieData.get(i)[4];
+							output = (String)movieData.get(i)[0];
+						}
+					}
+				}
+			}
+			System.out.println(output);
+			flag = 0;
+			output = "";
+
 	}
+
 }
